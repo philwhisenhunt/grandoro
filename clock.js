202622fc318d63fd751d7bf9c3d6fb087300d2c8
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-06 10:07:38 -0400

    finished step 01

--- clock.js
+++ clock.js
@@ -1,3 +1,10 @@
-console.log("hello");
 let time = 0;
 console.log(time);
+
+const refresh = () =>{
+    time++;
+    console.log(time);
+}
+//this causes the function refresh to happen every second (1000ms)
+const tick = () => setInterval(refresh, 1000);
+tick();
