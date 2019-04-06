e99ae9640f15c077813838cd7710ad101a24bedb
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-06 10:20:05 -0400

    added event listener for click

--- clock.js
+++ clock.js
@@ -1,10 +1,16 @@
 let time = 0;
-console.log(time);
+let paused = false;
+const ticker = document.getElementById('ticker');
+const playToggleBtn = document.getElementById('pause');
 
-const refresh = () =>{
-    time++;
-    console.log(time);
-}
+const refresh = () => {
+    //if and only if NOT paused, then keep the timer going
+    if (!paused){
+        time++;
+        ticker.innerHTML = time;
+    }
+};
+
 //this causes the function refresh to happen every second (1000ms)
 const tick = () => setInterval(refresh, 1000);
 tick();
