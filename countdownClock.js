08181c979147f1003f3500fd1df4df8e8b59f94e
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-07 15:41:34 -0400

    Building out framework

--- countdownClock.js
+++ countdownClock.js
@@ -1,8 +1,15 @@
 console.log("working");
 
 let time = 35*60;
+let paused = false;
+let working = true;
 
 const refresh = () => {
+    if(!paused){
+        if(working){
+            
+        }
+    }
     time--;
     ticker.innerHTML = time;
    
