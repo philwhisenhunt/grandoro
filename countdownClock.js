c6319457eed18b20f6fffbaf382629f2e42d32d7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-07 15:37:51 -0400

    clock now ticking down in countdownClock.js

--- countdownClock.js
+++ countdownClock.js
@@ -1,1 +1,12 @@
 console.log("working");
+
+let time = 35*60;
+
+const refresh = () => {
+    time--;
+    ticker.innerHTML = time;
+   
+}
+
+const tickDown = () =>setInterval(refresh, 1000);
+tickDown();
