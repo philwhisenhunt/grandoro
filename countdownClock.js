da393c1f1e1c46c9fa5a7a68d85a90e39ed15631
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-08 19:03:10 -0400

    clock counting down

--- countdownClock.js
+++ countdownClock.js
@@ -1,17 +1,53 @@
-console.log("working");
+//make 5 minutes in seconds
+let realTime = 60*5;
+//use pad start?
 
-let time = 35*60;
+let time = (realTime);
+
+
+
+let workTime = 25;
+let breakTime = 5;
+let leftover = time % 60;
+let minutesLeft = Math.floor(time/60);
+console.log(minutesLeft);
+//gets the time to start at the right thing
+// ticker.innerHTML = time;
+tickerMinutes.innerHTML = minutesLeft;
+tickerSeconds.innerHTML = leftover;
+
 let paused = false;
 let working = true;
+const playToggleBtn = document.getElementById("pause");
+playToggleBtn.addEventListener("click", () => (paused = !paused));
 
 const refresh = () => {
     if(!paused){
-        if(working){
+        time--;
+
+       
+        let leftover = time % 60;
+        let minutesLeft = Math.floor(time/60);
+        tickerMinutes.innerHTML = minutesLeft;
+        tickerSeconds.innerHTML = leftover;
+        console.log("time is "+ time);
+        console.log("minutesLeft is " + minutesLeft);
+        console.log("leftover is" + leftover);  
+
+        if(time <= 0){
             
+            time = 0;
+            // ticker.innerHTML = time;
+            document.body.style.backgroundColor = "red";
+
+
         }
     }
-    time--;
-    ticker.innerHTML = time;
+
+    else{
+       
+
+    }
    
 }
 
