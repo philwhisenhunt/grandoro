a86d60320bb948ab78e323179f3dd483ca0b695c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-04-11 10:40:50 -0400

    Now timer switching to break and counting down on page

--- countdownClock.js
+++ countdownClock.js
@@ -1,15 +1,21 @@
 //make 5 minutes in seconds
 //let realTime = 60*5;
-let realTime = 5;
+let realTime = 2;
 
 //use pad start?
 
 let time = (realTime);
+const addIt = document.getElementById("breakall");
+const removeIt = document.getElementById("clockshow");
+
 
 
 
 let workTime = 25;
 let breakTime = 5;
+
+//make a working or breaking status, or make a third one for paused. 
+//make as a string
 let leftover = time % 60;
 let minutesLeft = Math.floor(time/60);
 console.log(minutesLeft);
@@ -25,38 +31,51 @@ playToggleBtn.addEventListener("click", () => (paused = !paused));
 
 const refresh = () => {
     if(!paused){
+        if(time>0){
         time--;
-        
+        console.log("time is: " + time);
+        //console.log("With pad start time is" + );
        
         let leftover = time % 60;
         let minutesLeft = Math.floor(time/60);
         tickerMinutes.innerHTML = minutesLeft;
+        console.log("The minutesLeft variable is " + minutesLeft);
         tickerSeconds.innerHTML = leftover;
+       }
+        /*
         console.log("time is "+ time);
         console.log("minutesLeft is " + minutesLeft);
         console.log("leftover is" + leftover);  
-
+        */
         if(time <= 0){
             
             time = 0;
             // ticker.innerHTML = time;
             document.body.style.backgroundColor = "red";
+            //paused = true;
 
-           
+            if(breakTime >= 0 && time == 0 ){
+                //addIt.style.visibility = "visible";
+                //removeIt.style.visibility = "hidden";
+                breakMinutes.innerHTML = Math.floor(time/60);
+                breakSeconds.innerHTML = breakTime % 60;
+
+                breakTime--;
+                console.log("breakTime is " + breakTime);
+                }
     
 
 
         }
+        
+
     }
 
     else{
        
 
     }
-    if(breakTime >= 0){
-        breakTime--;
-        console.log("breakTime is " + breakTime);
-        }
+    
 }
 
 const tickDown = () =>setInterval(refresh, 1000);
